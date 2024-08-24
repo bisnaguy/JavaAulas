c6500474e8d9c2e4289925beea18b50dffc73fae
Gabriel <azambujagabriel90@gmail.com>
2024-08-24 11:00:21 -0300

    Commit third

---
+++ trilha-Java-basico/java-anatomia-de-classes/src/Somar.java
@@ -0,0 +1,15 @@
+public class Somar {
+    public static void main(String[] args) {
+        int primeiroNumero = 5;
+        int segundoNumero = 6;
+
+        int somar = somar(primeiroNumero,segundoNumero);
+        System.out.println(somar);
+    }
+
+    public static int somar(int primeiroNumero, int segundoNumero) {
+        
+        
+        return primeiroNumero + segundoNumero;
+    }
+}
